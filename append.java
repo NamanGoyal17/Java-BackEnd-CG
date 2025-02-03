2eeb4ff94ea539845399fe1ff4bb5fcef3c8a43f
Naman Goyal <namangoyal.com@gmail.com>
2025-02-03 13:52:05 +0530

    Naman added : concation of two string

---
+++ append.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        String str1 = "arc";
+        String str2 = "car";
+        StringBuffer string = new StringBuffer();
+        string.append(str1);
+        string.append(str2);
+        System.out.println(string);
+    }
+}
